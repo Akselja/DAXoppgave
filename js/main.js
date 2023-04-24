709fffe12a487213eec974bbf59e72a7f6b280f1
Aksel <aksel4455@gmail.com>
2023-04-24 17:22:09 +0200

    Added a clock and some styles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 // DOM
 const nameForm = document.querySelector("form");// skjemaet
 const container = document.querySelector(".container");// container for velkommen-meldingen
+const timeContainer = document.querySelector(".time");// container for tiden
 const history = document.querySelector(".history");// container for registrerte navn
+
+
 
 // sjekker hvis det er noen "Names" variabler i local storage, legger det til siden hvis ja, oppretter tom array hvis nei
 if (localStorage.getItem("Names")) {
@@ -12,6 +15,7 @@ if (localStorage.getItem("Names")) {
 } else {
     localStorage.setItem("Names", JSON.stringify([]));
 }
+
 
 
 // event listener for skjemaet
@@ -31,9 +35,11 @@ nameForm.addEventListener("submit", async e => {
 
     const nameList = JSON.parse(localStorage.getItem("Names"));
     nameList.forEach(e => {
-        history.innerHTML += `<div class="entry">${e}</div>`
+        history.innerHTML += `<div class="entry">${e}</div>`;
     })
 })
+
+
 
 // funksjon for å legge til et navn i listen, dette bruker JSON.parse og JSON.stringify, fordi local storage bare støtter strings og ikke noe annet.
 const LSAddItem = (item) => {
@@ -41,3 +47,23 @@ const LSAddItem = (item) => {
     nameList.push(item);
     localStorage.setItem("Names", JSON.stringify(nameList));
 }
+
+
+
+// funksjon for å telle tiden, og sende den til nettsiden
+const countTime = () => {
+    let currentTime = new Date();
+    console.log(currentTime.getHours(), currentTime.getMinutes(), currentTime.getSeconds());
+    timeContainer.innerText = `${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`
+
+    if (currentTime.getSeconds() == 0) {
+        
+        const randomNumber = Math.floor(Math.random() * 10);
+        alert(randomNumber);
+    }
+}
+
+countTime();
+setInterval(() => {
+    countTime();
+}, 100);
